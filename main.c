d7938eeea9b4a62563786e3ed82faae13b842db6
Dymea <dxs5940@psu.edu>
2020-09-17 22:59:41 +0000

    changed if statement in void function

--- main.c
+++ main.c
@@ -10,24 +10,23 @@
 #include <stdlib.h>
 
 int sum_n(int n) {
-  if (n >= 0) {
+  if (n >= 1) {
     return 1;
     } else {
       return n+sum_n(n-1);
     }
   }
 
-void print_n(char *s, int n) {
-  if (n <= 0) {
+void print_n(char const *s, int n) {
+  if (n != 0) {
     printf(s,n);
     print_n(s,n-1);
   }
 }
 
 int main(void) {
-  char n = readline(atoi("Enter an int: ");
+  int n = atoi(readline("Enter an int: "));
   printf("sum is %i \n", sum_n(n)); 
-  char *s = readline("Enter a string: ");
+  char const *s = readline("Enter a string: ");
   print_n(s, n);
-  return 0;
 } 
